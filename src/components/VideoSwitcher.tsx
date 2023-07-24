e4feb2266e252c2dcc9d7ef0fe8d3665aef06f87
Mohab <mohabsameh@outlook.com>
2023-07-24 15:48:52 +0300

    updated deprecated useColorMode usage

--- src/components/VideoSwitcher.tsx
+++ src/components/VideoSwitcher.tsx
@@ -1,20 +1,27 @@
-import React from 'react';
-import { useColorMode } from '@docusaurus/theme-common';
-import ReactPlayer from 'react-player';
+import React from "react";
+import { useColorMode } from "@docusaurus/theme-common";
+import ReactPlayer from "react-player";
 
 interface Props {
   lightImageSrc: string;
   darkImageSrc: string;
 }
 
-const ImageSwitcher = ({lightImageSrc, darkImageSrc}: Props) => {
-  const { isDarkTheme } = useColorMode();
+const ImageSwitcher = ({ lightImageSrc, darkImageSrc }: Props) => {
+  const isDarkTheme = useColorMode().colorMode === "dark";
 
   return (
     <>
-      <ReactPlayer playing muted={true} loop={true} width='100%' height='100%' url={isDarkTheme ? darkImageSrc : lightImageSrc} />
+      <ReactPlayer
+        playing
+        muted={true}
+        loop={true}
+        width="100%"
+        height="100%"
+        url={isDarkTheme ? darkImageSrc : lightImageSrc}
+      />
     </>
-  )
-}
+  );
+};
 
 export default ImageSwitcher;
